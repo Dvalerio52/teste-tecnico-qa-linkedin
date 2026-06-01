6e3adfbcdc6c95321fd3137d9c6e0e0c50a25ee8
Douglas Valerio <dvalerio52@gmail.com>
2026-06-01 13:47:13 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.linkedin.com',
+    supportFile: false, // Diz ao Cypress que não precisamos do arquivo de suporte
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    chromeWebSecurity: false,
-    viewportWidth: 1280,
-    viewportHeight: 720
   },
 });
